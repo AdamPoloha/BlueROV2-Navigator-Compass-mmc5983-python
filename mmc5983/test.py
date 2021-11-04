0036b95cae6debb86b2fcf28c24a7b8d1b174289
Jacob Walser <jwalser90@gmail.com>
2021-11-04 21:35:30 +0000

    test: cleanup

--- mmc5983/test.py
+++ mmc5983/test.py
@@ -15,8 +15,6 @@ def main():
         def data_getter():
             data = mmc.measure()
             return f"{data.x_raw} {data.y_raw} {data.z_raw} {data.t_raw} {data.x:.6f} {data.y:.6f} {data.z:.6f} {data.t:.3f}"
-            # return f"{data.x_raw} {data.y_raw} {data.z_raw} {data.t_raw} {bin(data.x_raw)} {bin(data.y_raw)} {bin(data.z_raw)} {bin(data.xtra)}"
-            return f"{data.x_raw:9} {bin(data.x_raw):25} {bin(data.xtra):12}"
 
         log.log_data_loop(data_getter, parser_args=args)
 
