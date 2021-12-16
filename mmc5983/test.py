a3941d5e27bea9325fa519ff9d89476426d46ab0
Jacob Walser <jwalser90@gmail.com>
2021-12-16 13:34:54 -0500

    test: calibrate sensor every 1 seconds

--- mmc5983/test.py
+++ mmc5983/test.py
@@ -1,23 +1,41 @@
 #!/usr/bin/python3
 
 def main():
-    from mmc5983 import MMC5983
     from llog import LLogWriter
+    from mmc5983 import MMC5983
+    import time
 
     device = "mmc5983"
     parser = LLogWriter.create_default_parser(__file__, device)
     parser.add_argument("--bus", type=int, default=None, help="i2c bus")
+    parser.add_argument("--cal", type=int, default=None, help="i2c bus")
     args = parser.parse_args()
 
 
     with LLogWriter(args.meta, args.output, console=args.console) as log:
         mmc = MMC5983(i2cbus=args.bus)
+        lastcal = time.time()
 
-        def data_getter():
-            data = mmc.measure()
-            return f"{data.x_raw} {data.y_raw} {data.z_raw} {data.t_raw} {data.x:.6f} {data.y:.6f} {data.z:.6f} {data.t:.3f}"
+        frequency = args.frequency
+        duration = args.duration
+        stop_on_error = args.stop_on_error
 
-        log.log_data_loop(data_getter, parser_args=args)
+        start_time = time.time()
+        while time.time() < start_time + duration:
+            try:
+                if time.time() > lastcal + 1:
+                    mmc.calibrate()
+                    lastcal = time.time()
+                data = mmc.measure()
+                logdata = f"{data.x_raw} {data.y_raw} {data.z_raw} {data.t_raw} {data.x:.6f} {data.y:.6f} {data.z:.6f} {data.t:.3f}"
+                log.log_data(logdata)
+            except Exception as e:
+                log.log_error(f'"{e}"')
+                if stop_on_error:
+                    return
+
+            if frequency:
+                time.sleep(1.0 / frequency)
 
 if __name__ == '__main__':
     main()
