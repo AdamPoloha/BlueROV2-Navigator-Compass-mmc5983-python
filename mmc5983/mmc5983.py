b100c105d709cdc283cfc63d8e5598ccec471989
Jacob Walser <jwalser90@gmail.com>
2021-12-19 23:22:06 -0500

    mmc5983: bugfix calibration, add continuous measurement and read_data function

--- mmc5983/mmc5983.py
+++ mmc5983/mmc5983.py
@@ -16,14 +16,21 @@ REG_CONTROL0_TMM = (1<<0)
 REG_CONTROL0_TMT = (1<<1)
 REG_CONTROL0_SET = (1<<3)
 REG_CONTROL0_RESET = (1<<4)
+REG_CONTROL0_AUTOSREN = (1<<5)
 
 # bits in REG_CONTROL1
 REG_CONTROL1_BW0 = (1<<0)
 REG_CONTROL1_BW1 = (1<<1)
 REG_CONTROL1_SW_RST = (1<<7)
+REG_CONTROL1_CONFIG = 0b11 # BW = 0b11 - 0.5ms per measurement
+
+REG_CONTROL2_CMFREQ = 0b111
+REG_CONTROL2_CMEN = (1<<4)
+REG_CONTROL2_ENPRDSET = (1<<7)
+REG_CONTROL2_CONFIG = 0b1111 # enable continuous measurement at 1kHz
 
 MIN_DELAY_SET_RESET = 1e-3
-MIN_DELAY_MEASURE = 1.6e-3
+MIN_DELAY_MEASURE = 1.1e-3
 
 MMC5883_ID = 0x0C
 
@@ -50,9 +57,9 @@ class CompassData:
         self.y_norm = self.y_raw/16384
         self.z_norm = self.z_raw/16384
 
-        self.x = self.x_norm + caldata[0]
-        self.y = self.y_norm + caldata[1]
-        self.z = self.z_norm + caldata[2]
+        self.x = self.x_norm - caldata[0]
+        self.y = self.y_norm - caldata[1]
+        self.z = self.z_norm - caldata[2]
 
         self.t_raw = rawdata[7]
         self.t = self.t_raw*200.0/256 - 75
@@ -63,21 +70,28 @@ class MMC5983:
         if i2cbus is None: # spi communication
             self._bus = spidev.SpiDev()
             self._bus.open(bus, cs)
-            self._bus.max_speed_hz = 10000000 # 10MHz
+            self._bus.max_speed_hz = 4000000 # 10MHz
         else: # i2c communication
             self._bus = smbus2.SMBus(i2cbus)
             self.read = self.readI2C
             self.write = self.writeI2C
 
         self.caldata = [0, 0, 0]
-        self.reset()
+        self.software_reset()
         self._id = self.read_id()
-        self.set_BW()
-        self.calibrate()
+        self.reset()
+        self.config1()
+        self.config2()
+
+    def config1(self):
+        self.write(REG_CONTROL1, [REG_CONTROL1_CONFIG])
+
+    def config2(self):
+        self.write(REG_CONTROL2, [REG_CONTROL2_CONFIG])
 
     def software_reset(self):
         self.write(REG_CONTROL1, [REG_CONTROL1_SW_RST])
-        time.sleep(0.01)
+        time.sleep(0.015)
 
     def reset(self):
         self.write(REG_CONTROL0, [REG_CONTROL0_RESET])
@@ -88,16 +102,21 @@ class MMC5983:
         time.sleep(MIN_DELAY_SET_RESET)
 
     def calibrate(self):
-        self.set()
-        setdata = self.measure()
-        self.reset()
-        resetdata = self.measure()
-        self.caldata = [
-            (setdata.x - resetdata.x)/2,
-            (setdata.y - resetdata.y)/2,
-            (setdata.y - resetdata.y)/2
-        ]
+        self.caldata = [0, 0, 0]
 
+        self.set()
+        time.sleep(MIN_DELAY_MEASURE)
+        setdata = self.read_data()
+
+        self.reset()
+        time.sleep(MIN_DELAY_MEASURE)
+        resetdata = self.read_data()
+
+        self.caldata = [
+            (setdata.x + resetdata.x)/2,
+            (setdata.y + resetdata.y)/2,
+            (setdata.z + resetdata.z)/2
+        ]
 
     def set_BW(self, BW=(REG_CONTROL1_BW0 | REG_CONTROL1_BW1)):
         self.write(REG_CONTROL1, [BW])
@@ -114,6 +133,9 @@ class MMC5983:
             status = self.readByte(REG_STATUS)
             continue
 
+        return self.read_data()
+
+    def read_data(self):
         rawdata = self.read(REG_XOUT_L, 8)
         return CompassData(rawdata, self.caldata)
 
